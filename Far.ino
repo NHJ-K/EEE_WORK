6222c0c9f4eead18e3d640bdadd39bacbc83f311
MOHAMED NIHAJ K <mohamed19130391@ssmpoly.ac.in>
2022-06-29 21:05:34 +0530

    Update Far.ino

--- Far.ino
+++ Far.ino
@@ -1,60 +1,118 @@
-#define BLYNK_TEMPLATE_ID "TMPL_KfnHFk1"
-#define BLYNK_DEVICE_NAME "DeltaTMPS"
-#define BLYNK_AUTH_TOKEN "mxlMDqA2Jq3vZVlToPYGQ5X7v0oGn_LK"
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_DEVICE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
 
 #define BLYNK_PRINT Serial
-#include <ESP8266WiFi.h>
+
+#define ON_BLYNK V3
+#define OFF_BLYNK V4
+
+int Gas_analog = 32;
+
+#include "EmonLib.h"   
+#include <WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp8266.h>
+#include <BlynkSimpleEsp32.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SPI.h>
+#include <ESP32Servo.h>
+Servo servo;
 
-#include <EmonLib.h> 
- 
-#define ANALOG_INPUT_SENSOR D2
 
-#define ONE_WIRE_BUS D1
+
+#define ONE_WIRE_BUS 2 //sensor pin D4
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-char ssid[] = "NIHAJ_HOME";  // type your wifi name
-char pass[] = "aeueyoaz@1";  // type your wifi password
+char ssid[] = "**********";  // type your wifi name
+char pass[] = "*********";  // type your wifi password
 
 BlynkTimer timer;
 
 EnergyMonitor emon1;
 
-long wattsum = 0;
-double kwh = 0;
-double wh = 0;
-long lastmillis = 0;
-int minutes = 0;
+EnergyMonitor emon;
+#define vCalibration 110
+#define currCalibration 5.3
 
-double sensorValue1 = 0;
-double sensorValue2 = 0;
-int crosscount = 0;
-int climb_flag = 0;
-int val[100];
-int max_v = 0;
-double VmaxD = 0;
-double VeffD = 0;
-double Veff = 0;
+void sendSensor();
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  //WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid, pass);
+  Blynk.begin(auth, ssid, pass);
+  servo.attach(5);
+  servo.write(40);
+  emon.voltage(34, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+  emon.current(35, currCalibration);
+  timer.setInterval(100L, sendSensor);
+  pinMode(Gas_analog  , INPUT);
+  Blynk.virtualWrite(V5,0);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
+
+//servo on off
+BLYNK_WRITE(V4) {
+  if(param.asInt() == 1){
+  servo.write(150);
+  delay(1000);
+  servo.write(40);
+  }
+}
+BLYNK_WRITE(V3) {
+  if(param.asInt() == 1){
+   servo.write(40);
+   delay(1000);
+   servo.write(0);
+  }
+}
 
 void sendSensor()
 {
+  //gas sensor
+  int gassensorAnalog = analogRead(Gas_analog);
+
+  Serial.print("Gas Sensor: ");
+  Serial.print(gassensorAnalog);
+  //Serial.print("\t");
+  //Serial.print("Gas Class: ");
+  //Serial.print(gassensorDigital);
+  //Serial.print("\t");
+  //Serial.print("\t");
+
+  if (gassensorAnalog > 1000) {
+    Serial.println("Gas");
+    //digitalWrite (Buzzer, HIGH) ; //send tone
+    Blynk.virtualWrite(V5,1);
+    delay(1000);
+    //digitalWrite (Buzzer, LOW) ;  //no tone
+  }
+  else {
+    Blynk.virtualWrite(V5,0);
+    Serial.println("No Gas");
+  }
+  delay(100);
+
+
   // Request temperature to all devices on the data line
-  sensors.requestTemperatures(); 
+  sensors.requestTemperatures();
 
   float tempC = sensors.getTempCByIndex(0);
-  
-  if(tempC != DEVICE_DISCONNECTED_C) 
+
+  if (tempC != DEVICE_DISCONNECTED_C)
   {
     Serial.print("Celsius temperature: ");
     Serial.println(tempC);
-  } 
+  }
   else
   {
     Serial.println("Error: Could not read temperature data");
@@ -63,91 +121,26 @@ void sendSensor()
   //int tempC=sensors.getTempCByIndex(0);
   //int tempF=sensors.getTempFByIndex(0);
   delay(1000);
-  
+
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
-    Blynk.virtualWrite(V0, tempC);
-    //Blynk.virtualWrite(V1, tempF);
-    delay(500);
+  Blynk.virtualWrite(V0, tempC);
+  //Blynk.virtualWrite(V1, tempF);
+  delay(500);
 
-
-  for ( int i = 0; i < 100; i++ ) {
-    sensorValue1 = analogRead(A0);
-    if (analogRead(A0) > 511) {
-      val[i] = sensorValue1;
-    }
-    else {
-      val[i] = 0;
-    }
-    delay(1);
-  }
-
-  max_v = 0;
-
-  for ( int i = 0; i < 100; i++ )
-  {
-    if ( val[i] > max_v )
-    {
-      max_v = val[i];
-    }
-    val[i] = 0;
-  }
-  if (max_v != 0) {
-
-
-    VmaxD = max_v;
-    VeffD = VmaxD / sqrt(2);
-    Veff = (((VeffD - 903.76) / -644.24) * -415.2) + 415.2;
-  }
-  else {
-    Veff = 0;
-  }
-  Serial.print("Voltage: ");
-  Serial.println(Veff);
-  if(Veff>185){
-  Blynk.virtualWrite(V1,Veff);
-  }
-  else{
+  emon.calcVI(20, 2000);
+  Serial.print("Vrms: ");
+   Serial.print(emon.Vrms,2);
+   Serial.print("V");
+   if(emon.Vrms>30){
+   Blynk.virtualWrite(V1, emon.Vrms);
+   }
+   else{
     Blynk.virtualWrite(V1,0);
-  }
-  VmaxD = 0;
+   }
+   Serial.print("\tIrms: ");
+    Serial.print(emon.Irms,4);
+    Serial.print("A");
+   Blynk.virtualWrite(V2.#,emon.Irms);
 
-
-  if (millis()-lastmillis > 5000) {
-    double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-    if (Irms < 0.3) Irms = 0;
-    long watt = Irms*240.0;
-    wattsum = wattsum+watt;
-    minutes = minutes + 1;
-    lastmillis = millis();
-    Serial.print(watt);         // Apparent power
-    Serial.print(" ");
-    Serial.println(Irms);     
-    Blynk.virtualWrite(V2,Irms);// Irms
-  
-  }
-  if (minutes == 10) {
-    wh = wh + wattsum/10;
-    kwh = wh/1000;
-    
-    wattsum = 0;
-    minutes = 0;
-  }
-    
-}
-
-void setup() {
-  Serial.begin(115200);
-  sensors.begin();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  Blynk.begin(auth, ssid, pass);
-  emon1.current(ANALOG_INPUT_SENSOR, 25); 
-  timer.setInterval(100L, sendSensor);
-}
-
-void loop() {
-    Blynk.run();
-    timer.run();
-    delay(100);
 }
